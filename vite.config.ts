bdd0f5b458eb51bbc7ccc19430ca587cf1551af0
kiteatkit <k_0_b12@protonmail.com>
2025-05-07 22:46:28 +0300

    1

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/site_c/',
+  base: '/resite/',
   server: {
     host: "::",
     port: 8080,
